802fe099ea3beff10e1baffeb3ebe095e7c0ee60
suhaniiz <suhanishrivastava75@gmail.com>
2026-06-07 02:11:01 +0530

    fix: restore missing milliseconds in log timestamps (#1148)

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -41,9 +41,7 @@ const format = winston.format.combine(
   winston.format.printf((info) => {
     const { timestamp, level, message, ...args } = info;
 
-    const ts = timestamp.slice(0, 19).replace("T", " ");
-
-    return `${ts} [${level}]: ${message} ${
+    return `${timestamp} [${level}]: ${message} ${
       Object.keys(args).length ? JSON.stringify(args, null, 2) : ""
     }`;
   })
